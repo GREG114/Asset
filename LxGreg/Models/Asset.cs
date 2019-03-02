0a95c53788a206cf743a988242def0106f45e125
超 刘 <greg_114@163.com>
2019-03-02 19:35:14 +0800

    建模

---
+++ LxGreg/Models/Asset.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LxGreg.Models
+{
+    public class Asset
+    {
+        public int Id { get; set; }
+        public string ItemNumber { get; set; }
+        public string ItemShortNumber { get; set; }
+        public string ItemName { get; set; }
+        public string Model { get; set; }
+        
+    }
+}
